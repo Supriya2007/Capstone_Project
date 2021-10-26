d6ad6096d34c793f6ddc9780f5b3283286394dd0
Supriya2007 <supriyakavi2016@gmail.com>
2021-10-26 23:35:08 +0530

    Test case changed

--- test_programs/control_statements_with_braces_test.c
+++ test_programs/control_statements_with_braces_test.c
@@ -1,23 +1,12 @@
 int main( )
-
    {
-
     int a;
-
     a=2;
-
     if(a>0)
-
       printf( "n The number %d is positive.",a);
-
     else
-
      {
-
       printf("n The number %d is negative.",a);
-
      }
-
   return 0;
-
    }
