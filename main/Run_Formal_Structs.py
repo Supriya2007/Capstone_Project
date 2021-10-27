c5eab7dec180cd1b0a6cf6dee654aaddc1220653
Supriya2007 <supriyakavi2016@gmail.com>
2021-10-27 11:24:39 +0530

    Changed name of dp guideline

--- main/Run_Formal_Structs.py
+++ main/Run_Formal_Structs.py
@@ -56,7 +56,8 @@ lib_rules = {
 'Check if linked list with tailer node concept is used' : 'list_tailer_fs.txt',
 'Check if tree data structure is used ' : 'tree_fs.txt',
 'Check if list iterator is used': 'list_iterator_fs.txt',
-'Check if dynamic programming has been used' : 'dp_fs.txt',
+'Check if bottom-up dynamic programming has been used' : 'dp_fs.txt',
+'Check if memoization is used' : 'memoization_fs.txt',
 }
 
 #Even code files without errors inserted into violations. Will have an empty list as value
